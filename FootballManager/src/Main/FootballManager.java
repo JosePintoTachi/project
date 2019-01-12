0f82ec09ed32028f980bef74913834d242bb0148
Jose Pinto <joseapinto41@gmail.com>
2019-01-12 18:57:09 +0000

    Trying commit 1st time

---
+++ FootballManager/src/Main/FootballManager.java
@@ -0,0 +1,13 @@
+
+package Main;
+
+/**
+ *
+ * @author Jose Pinto
+ */
+public class FootballManager {
+    public static void main(String[] args) {
+        System.out.println("Does this work?");
+    }
+    
+}
